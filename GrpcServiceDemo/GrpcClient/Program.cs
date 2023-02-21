941f081d35fd239a56f60d712103e11aca45d5c8
Hardik Kishorbhai Panchal <Hardik_Panchal@ad.infosys.com>
2023-02-21 09:59:53 +0530

    Adding full code

---
+++ GrpcServiceDemo/GrpcClient/Program.cs
@@ -0,0 +1,39 @@
+﻿using Grpc.Net.Client;
+using GrpcClient;
+
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var client = new CountryInfoClient();
+
+        Console.Write("Enter Country:");
+        string inputCountry = Console.ReadLine().Trim();
+
+        while (inputCountry.ToLower().Trim() != "exit")
+        {
+            await client.CallGrpcClient(inputCountry);
+            Console.Write("\nEnter Exit to stop. \nEnter Country:");
+            inputCountry = Console.ReadLine().Trim();
+        }                
+    }
+}
+
+
+public class CountryInfoClient
+{
+    public async Task CallGrpcClient(string reqCountry)
+    {
+        var grpcChannel = GrpcChannel.ForAddress("https://localhost:7125");
+
+        var client = new CountryInfo.CountryInfoClient(grpcChannel);
+
+        var request = new CountryCapitalRequest();
+        request.Country = reqCountry;
+
+        var response = await client.GetCountryCapitalAsync(request);
+        Console.WriteLine($"Country: {response.Country}, Capital: {response.City}");
+
+    }
+}
---
+++ GrpcServiceDemo/GrpcService/Services/CountryInfoService.cs
@@ -0,0 +1,31 @@
+﻿using Grpc.Core;
+using Newtonsoft.Json;
+
+namespace GrpcService.Services
+{
+    public class CountryInfoService : CountryInfo.CountryInfoBase
+    {
+        public override Task<CountryCapitalResponse> GetCountryCapital(CountryCapitalRequest request, ServerCallContext context)
+        {
+            List<CapitalCity> cities = new();
+            using (StreamReader r = new StreamReader(@"Assets\\CapitalCities.json"))
+            {
+                string json = r.ReadToEnd();
+                cities = JsonConvert.DeserializeObject<List<CapitalCity>>(json);
+            }
+            var response = cities?.FirstOrDefault(c => c.Country.ToLower() == request.Country.ToLower());
+            if(response is null)
+            {
+                return Task.FromResult(new CountryCapitalResponse() { City = "Capital info not found", Country = request.Country });
+            }
+
+            return Task.FromResult(new CountryCapitalResponse() { City = response.City, Country = response.Country });
+
+        }
+    }
+    public class CapitalCity
+    {
+        public string Country { get; set; }
+        public string City { get; set; }
+    }
+}
